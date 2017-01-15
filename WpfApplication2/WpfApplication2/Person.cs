d5263c90cd353555badde0164b8ca3091d6a4bb6
ImreLajtos <lajti00@gmail.com>
2017-01-15 13:19:39 +0100

    First comit test with WPF applications

---
+++ WpfApplication2/WpfApplication2/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+
+    }
+
+    public static class PersonManager
+    {
+        private static readonly Lazy<ObservableCollection<Person>> personHolder =
+            new Lazy<ObservableCollection<Person>>
+            (
+                () =>
+                {
+                    var r = new Random();
+                    return new ObservableCollection<Person>(
+                        Enumerable.Range(0, 5).Select(
+                            i => new Person { Id = Guid.NewGuid(), Age = r.Next(), Name = "Person" + i.ToString() }
+                            )
+                        );
+                },
+                true
+            );
+
+        public static ObservableCollection<Person> PersonList
+        {
+            get { return personHolder.Value; }
+        }
+
+    }
+}
